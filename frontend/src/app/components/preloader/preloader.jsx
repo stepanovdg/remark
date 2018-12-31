70e258b81008052248f2bebd9f7d13410be611e2
igoradamenko <mail@igoradamenko.com>
2018-12-31 23:21:54 +0300

    add preloader component

---
+++ frontend/src/app/components/preloader/preloader.jsx
@@ -0,0 +1,9 @@
+const Preloader = props => (
+  <div className={b('preloader', props)}>
+    <div className="preloader__bounce" />
+    <div className="preloader__bounce" />
+    <div className="preloader__bounce" />
+  </div>
+);
+
+export default Preloader;
