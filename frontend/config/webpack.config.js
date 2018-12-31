6cacdd92bdca50e1a7d43e9c2488788228fe6a2e
igoradamenko <mail@igoradamenko.com>
2018-12-31 20:57:01 +0300

    add support of scss files

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -3,6 +3,9 @@ const Define = webpack.DefinePlugin;
 const Clean = require('clean-webpack-plugin');
 const Html = require('html-webpack-plugin');
 
+const sass = require('sass');
+const Fiber = require('fibers');
+
 const path = x => (require('path')).resolve(__dirname, '..', ...x.split('/'));
 
 const env = process.env.NODE_ENV || 'development';
@@ -30,12 +33,54 @@ module.exports = {
       'react-dom': path('./node_modules/preact-compat'),
     },
   },
+  resolveLoader: {
+    alias: {
+      'scss-vars-loader': path('config/loaders/scss-vars-loader'),
+    },
+  },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('autoprefixer')({
+                  browsers: ['> 1%', 'android >= 4.4.4', 'ios >= 9'],
+                }),
+                require('postcss-url')({
+                  url: 'inline',
+                  maxSize: 5,
+                }),
+              ],
+            },
+          },
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: sass,
+              fiber: Fiber,
+              sourceMap: true,
+              includePaths: [path('src')],
+            },
+          },
+          'scss-vars-loader',
+        ],
       },
     ],
   },
