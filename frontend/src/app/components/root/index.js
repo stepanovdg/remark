c0481c670e817d0ebb5bc76b5db882e05e8beb4d
igoradamenko <mail@igoradamenko.com>
2018-12-31 23:22:24 +0300

    add root_loading

--- frontend/src/app/components/root/index.js
+++ frontend/src/app/components/root/index.js
@@ -1,3 +1,23 @@
-export { default } from './root';
+import { connect } from 'redux-zero/preact';
+
+import { changeTheme, increaseLoadingCounter, decreaseLoadingCounter } from 'app/store/actions';
+
+import Root from './root';
+
+const mapStateToProps = state => ({
+  isLoading: state.loadingCounter > 0,
+});
+
+const mapActionsToProps = {
+  increaseLoadingCounter,
+  decreaseLoadingCounter,
+  changeTheme,
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(Root);
 
 require('./root.scss');
+
+require('./__preloader/root__preloader.scss');
+
+require('./_loading/root_loading.scss');
--- frontend/src/app/components/root/root.jsx
+++ frontend/src/app/components/root/root.jsx
@@ -1,8 +1,22 @@
+import Preloader from 'app/components/preloader';
+
 export default class Root extends Component {
+  componentWillMount() {
+    // emulation of loading
+    this.props.increaseLoadingCounter();
+    setTimeout(() => this.props.decreaseLoadingCounter(), 2000);
+  }
+
   render(props, state) {
+    const { isLoading } = props;
+
     return (
-      <div className={b('root', props)}>
-        remark42
+      <div className={b('root', props, { loading: isLoading })}>
+        <div className="root__body">
+          remark42
+        </div>
+
+        <Preloader mix="root__preloader"/>
       </div>
     );
   }
