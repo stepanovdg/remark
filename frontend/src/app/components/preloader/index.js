fb80dea3cdc8dbbe543cca58390c3d9e69890114
igoradamenko <mail@igoradamenko.com>
2019-01-01 17:37:11 +0300

    add themes support to preloader component

--- frontend/src/app/components/preloader/index.js
+++ frontend/src/app/components/preloader/index.js
@@ -3,3 +3,6 @@ export { default } from './preloader';
 require('./preloader.scss');
 
 require('./__bounce/preloader__bounce.scss');
+
+require('./_theme/_light/preloader_theme_light.scss');
+require('./_theme/_dark/preloader_theme_dark.scss');
--- frontend/src/app/components/preloader/preloader.jsx
+++ frontend/src/app/components/preloader/preloader.jsx
@@ -1,9 +1,15 @@
-const Preloader = props => (
-  <div className={b('preloader', props)}>
-    <div className="preloader__bounce" />
-    <div className="preloader__bounce" />
-    <div className="preloader__bounce" />
-  </div>
-);
+import withTheme from 'app/components/with-theme';
 
-export default Preloader;
+class Preloader extends Component {
+  render(props) {
+    return (
+      <div className={b('preloader', props)}>
+        <div className="preloader__bounce"/>
+        <div className="preloader__bounce"/>
+        <div className="preloader__bounce"/>
+      </div>
+    );
+  }
+}
+
+export default withTheme(Preloader);
