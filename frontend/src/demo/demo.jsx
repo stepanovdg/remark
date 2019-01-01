3f05df26ef407db03026120a26800778be11f871
igoradamenko <mail@igoradamenko.com>
2019-01-01 19:16:27 +0300

    make it possible to use theme query param to set demo page theme

--- frontend/src/demo/demo.jsx
+++ frontend/src/demo/demo.jsx
@@ -1,10 +1,15 @@
 import { render } from 'preact';
 
+import getQueryParams from 'app/helpers/get-query-params';
+
 import App from '../app/app';
 
+const theme = getQueryParams().theme || 'light';
+
 window.DEMO = {
-  theme: 'light',
+  theme,
   render: () => {
+    document.querySelector('body').className = theme;
     document.getElementById('remark42').innerHTML = '';
 
     render(
