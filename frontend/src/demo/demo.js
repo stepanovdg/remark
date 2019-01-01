909f8e8d6ad65e3fa9e79e3313c2aeedbe56c2a2
igoradamenko <mail@igoradamenko.com>
2019-01-01 17:26:11 +0300

    improve demo page to toggle between light and dark theme

--- frontend/src/demo/demo.js
+++ frontend/src/demo/demo.js
@@ -2,7 +2,16 @@ import { render } from 'preact';
 
 import App from '../app/app';
 
-render(
-  <App/>,
-  document.getElementById('remark42')
-);
+window.DEMO = {
+  theme: 'light',
+  render: () => {
+    document.getElementById('remark42').innerHTML = '';
+
+    render(
+      <App theme={window.DEMO.theme}/>,
+      document.getElementById('remark42')
+    );
+  },
+};
+
+window.DEMO.render();
